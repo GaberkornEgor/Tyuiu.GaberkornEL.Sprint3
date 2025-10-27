ed6871ce13461cdc8da805745892bf1d88c334d1
Ymir4ik <middem4@gmail.com>
2025-10-27 14:38:03 +0500

    Add project files.

---
+++ Tyuiu.GaberkornEL.Sprint2.Task0.V8.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GaberkornEL.Sprint3.Task0.V8.Lib
+{
+    public class DataService : ISprint3Task0V8
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            double res = 1;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                res *= ((Math.Pow(value, i) + 1) * Math.Cos(4));
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint2.Task0.V8.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GaberkornEL.Sprint3.Task0.V8.Lib;
+namespace Tyuiu.GaberkornEL.Sprint3.Task0.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double value = 0.25;
+            int startValue = 1, stopValue = 10;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            Assert.AreEqual(0.019, res);
+        }
+    }
+}
