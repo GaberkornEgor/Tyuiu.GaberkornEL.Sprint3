cf3d05f92f98f6be9aeae466d3e864ed86452b88
Ymir4ik <middem4@gmail.com>
2025-10-28 19:39:39 +0500

    f

---
+++ Tyuiu.GaberkornEL.Sprint3.Task4.V27.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GaberkornEL.Sprint3.Task4.V27.Lib
+{
+    public class DataService : ISprint3Task4V27
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double y = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0) continue;
+                else y += x / (Math.Cos(x) + Math.Sin(x));
+            }
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint3.Task4.V27.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GaberkornEL.Sprint3.Task4.V27.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint3.Task4.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            int a = -5, b = 5;
+            var res = ds.Calculate(a, b);
+            Assert.AreEqual(-44.313, res);
+        }
+    }
+}
