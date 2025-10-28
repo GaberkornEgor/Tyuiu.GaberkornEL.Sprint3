bf623f13ccf16698e826a6b44ecebca08c08444f
Ymir4ik <middem4@gmail.com>
2025-10-28 22:12:19 +0500

    f

---
+++ Tyuiu.GaberkornEL.Sprint3.Task6.V28.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.GaberkornEL.Sprint3.Task6.V28.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint3.Task6.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            Assert.AreEqual(15, ds.GetSumTheDivisors(13, 19));
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint3.Task6.V28/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.GaberkornEL.Sprint3.Task6.V28.Lib;
+namespace Tyuiu.GaberkornEL.Sprint3.Task6.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("введите начальное значение отрезка: ");
+            int startValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("введите конечное значение отрезка: ");
+            int stopValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetSumTheDivisors(startValue, stopValue);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
